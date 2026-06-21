4d6f5deaac13ba862f33469d09b265417ee8ab7e
PUSHEENKING <3q3q14208@gmail.com>
2026-06-21 15:47:30 +0800

    add watchlist feature

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ from linebot.v3.messaging import (
     TextMessage
 )
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
-from stock import get_stock_price, report_watchlist
-
+from stock import get_stock_price
+from stock import report_watchlist
 app = Flask(__name__)
-
-CHANNEL_SECRET = ""
-CHANNEL_ACCESS_TOKEN = ""
+CHANNEL_SECRET = "eff61e65b8b788cf861aa0449464d133"
+CHANNEL_ACCESS_TOKEN = "joqleacwwZM0GSgthvivD8utdps84j4urkCIuOusNVCzEC1F0Z2mfKo+rSrTDpyASsHSAYF3aqTDqEe6PewCaTjKmSXU5h+AypfmzLztpWTy/v32paTZDfv5aHftR/DsT+f6lJlFm8S6uE9lsaUOMAdB04t89/1O/w1cDnyilFU="
 
 configuration = Configuration(
     access_token=CHANNEL_ACCESS_TOKEN
 )
+
 handler = WebhookHandler(CHANNEL_SECRET)
 
 @app.route("/")
@@ -29,20 +29,15 @@ def callback():
     signature = request.headers["X-Line-Signature"]
     body = request.get_data(as_text=True)
     handler.handle(body, signature)
-
     return "OK"
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     text = event.message.text.strip()
-    msg = text.lower()
-
-    if msg == "watchlist":
+    if text.lower() == "report":
         reply_text = report_watchlist()
-
     else:
         result = get_stock_price(text)
-
         if result:
             reply_text = (
                 f"{text}\n"
@@ -50,8 +45,7 @@ def handle_message(event):
                 f"漲跌幅：{result['percent']}%"
             )
         else:
-            reply_text = "查無資料，請輸入股票代碼，例如：2330，或輸入 watchlist"
-
+            reply_text = "查無資料"
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message(
@@ -65,3 +59,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
