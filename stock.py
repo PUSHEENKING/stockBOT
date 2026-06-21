d11b86e5ec9bfada7d6f0b0ed99cfb94081d2f0d
PUSHEENKING <3q3q14208@gmail.com>
2026-06-21 16:59:05 +0800

    update watchlist

--- stock.py
+++ stock.py
@@ -1,24 +1,25 @@
 import yfinance as yf
 
-if stock_id == "0000":
-    symbol = "^TWII"
-else:
-    symbol = f"{stock_id}.TW"
-watchlist = ["0000","2330","0050","00631L","2327","2383","3532","2303","2344"]
+watchlist = ["^TWII","2330","0050","00631L","2327","2383","3532","2303","2344"]
 
 def get_stock_price(stock_id):
-    stock = yf.Ticker(stock_id + ".TW")
+    if stock_id == "^TWII":
+        stock = yf.Ticker("^TWII")
+    else:
+        stock = yf.Ticker(stock_id + ".TW")
     data = stock.history(period="2d")
+
     if len(data) < 2:
         return None
     current = float(data["Close"].iloc[-1])
     previous = float(data["Close"].iloc[-2])
+
     percent = ((current - previous) / previous) * 100
+
     return {
         "price": round(current, 2),
         "percent": round(percent, 2)
     }
-
 def report_watchlist():
     result_text = ""
     for stock in watchlist:
